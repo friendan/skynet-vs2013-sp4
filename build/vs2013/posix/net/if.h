8cd1edc0bbf433102b6da750bde766050fc548fe
friendan <407583729@qq.com>
2022-11-10 00:33:50 +0800

    add net/if.h

---
+++ build/vs2013/posix/net/if.h
@@ -0,0 +1,57 @@
+#ifndef _QKC_NET_IF_H
+#define _QKC_NET_IF_H 1
+#include <quark_compile.h>
+#include <stdint.h>
+
+__BEGIN_DECLS
+enum
+{
+	IFF_UP = 0x1,  // Interface is up
+#define IFF_UP IFF_UP
+	IFF_BROADCAST = 0x2, // Broadcast address valid
+# define IFF_BROADCAST IFF_BROADCAST
+	IFF_DEBUG = 0x4,  // Turn on debugging
+# define IFF_DEBUG IFF_DEBUG
+	IFF_LOOPBACK = 0x8,  // Is a loopback net
+# define IFF_LOOPBACK IFF_LOOPBACK
+	IFF_POINTOPOINT = 0x10, // Interface is point- to-point link
+# define IFF_POINTOPOINT IFF_POINTOPOINT
+	IFF_NOTRAILERS= 0x20, /* Avoid use of trailers. */
+# define IFF_NOTRAILERS IFF_NOTRAILERS
+	IFF_RUNNING = 0x40, // Resources allocated
+# define IFF_RUNNING IFF_RUNNING
+	IFF_NOARP = 0x80, // No address resolution protocol
+# define IFF_NOARP IFF_NOARP
+	IFF_PROMISC = 0x100,  // Receive all packets
+# define IFF_PROMISC IFF_PROMISC
+
+	// Not supported
+	IFF_ALLMULTI = 0x200, //  Receive all multicast packets. * /
+# define IFF_ALLMULTI IFF_ALLMULTI
+
+	IFF_MASTER = 0x400, // Master of a load balancer
+# define IFF_MASTER   IFF_MASTER
+	IFF_SLAVE = 0x800, // Slave of a load balancer
+# define IFF_SLAVE IFF_SLAVE
+	IFF_MULTICAST = 0x1000, /* Supports multicast. */
+# define IFF_MULTICAST IFF_MULTICAST
+	IFF_PORTSEL = 0x2000, // Can set media type
+# define IFF_PORTSEL IFF_PORTSEL
+						 IFF_AUTOMEDIA = 0x4000, // Auto media select active
+# define IFF_AUTOMEDIA IFF_AUTOMEDIA
+	IFF_DYNAMIC = 0x8000, // Dialup device with changing addresses
+# define IFF_DYNAMIC IFF_DYNAMIC
+	IFF_LOWER_UP = 0x10000, /* driver signalsL1 up */
+# define IFF_LOWER_UP IFF_LOWER_UP
+	IFF_DORMANT = 0x20000   // driver signals dormant 
+# define IFF_DORMANT  IFF_DORMANT
+};
+
+struct if_nameindex
+{
+	unsigned int if_index;
+	char* if_name;
+};
+__END_DECLS
+#endif 
+
---
+++ build/vs2013/posix/quark_compile.h
@@ -0,0 +1,79 @@
+#ifndef QUARK_COMPILE_H
+#define QUARK_COMPILE_H 1
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef __QUARK_LIBC__
+#define __QUARK_LIBC__
+#endif
+
+#if defined(_M_IX86)
+#define _i386_
+#endif
+
+#if defined(_M_AMD64)
+#define __amd64__
+#define _AMD64_
+#endif
+
+#if defined(_M_X64)
+#define __x86_64__
+#endif
+
+//#define_ GNUC_
+//#define_ STDC_ VERSION_ 1 99409L
+//#define_ STDC_ VERSION_ 199901L
+#define __asm__  __asm
+
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL 0
+#else
+#define NULL ((void *)0)
+#endif
+#endif
+
+#ifndef __cplusplus
+typedef unsigned int size_ t;
+typedef unsigned short wchar_ t;
+typedefint ssize_ t;
+#endif
+
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS }
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
+
+#ifndef QUARK_IMPORT
+#define QUARK_IMPORT __declspec(dllimport)
+#endif
+
+#ifndef QUARK_EXPORT
+#define QUARK_EXPORT __declspec(dllexport)
+#endif
+
+#ifndef QUARK_LINKAGE
+//适用于用于导入第三库，而不是本地实现，但本地需要提供接口的函数
+#define QUARK_LINKAGE QUARK_IMPORT
+#endif
+
+#ifdef QKC_EXPORTS
+#define QKCAPI QUARK_EXPORT
+#else
+#define QKCAPI QUARK_IMPORT
+#endif
+
+#ifdef QKN_EXPORTS
+#define QKNAPI QUARK_EXPORT
+#else
+#define QKNAPI QUARK_IMPORT
+#endif
+#endif //QUARK_COMPILE_H
+
+
+
+
